35903a5bf8f2127d11f012be0e5c4a96f63b398d
issa <issabathily388@gmail.com>
2025-02-12 21:43:54 +0100

    version 2

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class POST(models.Model):
     title = models.CharField(max_length=50)
-    contener = models.TextField(max_length=200)
+    contener = models.CharField(max_length=500)
+
     created_at = models.DateTimeField( auto_now_add= True  )
     
     def __str__(self):
